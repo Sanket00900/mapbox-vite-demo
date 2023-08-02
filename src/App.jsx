426481020f5beb9e11a10f527ac988b2ce36f300
“Sanket <sanketsj00900@gmail.com>
2023-08-02 12:31:02 +0530

    env vars added

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.css";
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lat, setLat] = useState(18.526551794097287);
+  const [lng, setLng] = useState(73.84751349320722);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+}
+
+export default App;
